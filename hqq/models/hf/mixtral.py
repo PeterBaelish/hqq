bb63d5cb2759b6deb905ec5ac7339ec67d8538b0
PeterBaelish <baelish104543xx@gmail.com>
2024-01-08 22:08:44 +0800

    fix bug

--- hqq/models/hf/mixtral.py
+++ hqq/models/hf/mixtral.py
@@ -41,7 +41,8 @@ class MixtralPatch(BasePatch):
 			layers[i].attention.wv = patch_fct(layers[i].attention.wv, patch_params['self_attn.v_proj'])
 			layers[i].attention.wo = patch_fct(layers[i].attention.wo, patch_params['self_attn.o_proj'])
 			''''''
-			
+		
+		for i in tqdm(range(int(len(layers)/32)), disable=not verbose):
 			layers[i].feed_forward.expert_gpu_w1 = patch_fct(layers[i].feed_forward.expert_gpu_w1, patch_params['block_sparse_moe.experts.w1'])
 			layers[i].feed_forward.expert_gpu_w2 = patch_fct(layers[i].feed_forward.expert_gpu_w2, patch_params['block_sparse_moe.experts.w2'])
 			layers[i].feed_forward.expert_gpu_w3 = patch_fct(layers[i].feed_forward.expert_gpu_w3, patch_params['block_sparse_moe.experts.w3'])
